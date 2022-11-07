bb7ee55b883ef773128fd890a04ec7f3995f52af
Samuel <samuel@pop-os.localdomain>
2022-11-06 23:20:58 -0300

    added cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const routes = require("./routes");
-
+const cors = require("cors");
 const app = express();
 
 require("./database");
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.listen(process.env.PORT || 3333);
